ed5eaa62d33c61350473cfdd8e28e5b81bc9fa5a
Brian Richu <richubrian427@gmail.com>
2025-08-06 14:21:57 +0300

    made changes in js document

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 
+  document.addEventListener("DOMContentLoaded",function (){
+    const form = document.getElementById('registration- form');
+    const feedbackDiv = document.getElementById('form-feedback');
 
-document.addEventListener('submit',(event)=>{
-event.preventDefault();
-const form = document.getElementById('registration-form');
- 
-const username = document.getElementById('username').value.trim();
-const email = document.getElementById('email').value.trim();
-const password = document.getElementById('password').value.trim();
+  form.addEventListener('submit', (event)=>{
+      event.preventDefault();
 
-let isValid = true;
-let messages = [];
+      let isValid = true;
+      let messages = [];
+    
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
 
-if (username.length < 3) {
-  isValid = false;
-  messages.push("Enter a valid Username");
-}
 
-if (!email.includes('@') || !email.includes('.')) {
-  isValid = false;
-  messages.push("Email must contain @ and .");
-}
 
-if (password.length < 8) {
-  isValid = false;
-  messages.push("Minimum password length is 8");
-}
-const feedback = document.getElementById('form-feedback');
-feedback.style.display = "block";
- if (isValid) {
-  feedback.textContent = "Registration successful!";
-  feedback.style.color = "#28a745";
+    if (username.length < 3) {
+      isValid = false;
+      messages.push("Enter a valid Username");
+    }
 
- }else{
-  feedback.innerHTML = messages.join("<br>");
-  feedback.style.color = "#dc3545";
- }
-})
+    if (!email.includes('@') || !email.includes('.')) {
+      isValid = false;
+      messages.push("Email must contain @ and .");
+    }
+
+    if (password.length < 8) {
+      isValid = false;
+      messages.push("Minimum password length is 8");
+    }
+
+    feedbackDiv.style.display = "block";
+    if (isValid) {
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745";
+
+    }else{
+      feedbackDiv.innerHTML = messages.join("<br>");
+      feedbackDiv.style.color = "#dc3545";
+    }
+  });
+  });
